1d8113369f58862d605ec7208e5e898bc1ce60a9
GearHead87 <hosanulislam87@gmail.com>
2024-04-27 09:14:58 +0600

    delete some old code

--- index.js
+++ index.js
@@ -101,25 +101,6 @@ async function run() {
             res.send(result);
         })
 
-
-        // app.put('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const userData = req.body;
-        //     console.log("UPDATE USER", id, userData)
-        //     const filter = { _id: new ObjectId(id) };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: {
-        //             name: userData.name,
-        //             email: userData.email
-        //         }
-        //     }
-        //     const result = await userCollection.updateOne(filter, updateDoc, options);
-        //     res.send(result);
-        // })
-
-
-
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
